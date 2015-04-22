35bc048f425d837c179873ba131c0a23817ea206
Travis Eubanks <tmeubanks113@gmail.com>
2015-04-22 13:50:56 -0500

    adds comment in intializer file

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -1,4 +1,5 @@
 # Be sure to restart your server when you modify this file.
 
 # Configure sensitive parameters which will be filtered from the log file.
+# Here is where you put anything you dont want to show when logging in terminal
 Rails.application.config.filter_parameters += [:password]
