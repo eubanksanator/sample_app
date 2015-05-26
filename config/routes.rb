88766cbdbfb6d79eb721b3f383200c1432af3123
Travis Eubanks <tmeubanks113@gmail.com>
2015-05-26 15:54:49 -0500

    adds stlyes and sessions controller with create and new and delete

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
-  get 'signup' => 'users#new'
-
   root 'static_pages#home'
+  get 'sessions/new'
+  get 'users/new'
+  get 'signup' => 'users#new'
   get 'help' => 'static_pages#help'
   get 'about' => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
+  get 'login' => 'session#new'
+  post 'login' => 'session#create'
+  delete 'logout' => 'session#destroy'
 
 resources :users
 
